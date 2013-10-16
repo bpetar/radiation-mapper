caa97f6b595655472e860ea18a4db6b31195007c
extra.pedestrian@gmail.com <extra.pedestrian@gmail.com@f9976004-d5a0-e912-63d6-d81f884c1428>
2013-10-16 14:15:39 +0000

    adding list

--- trunk/src/com/mystic/peanut/radiation/RadiationMapperActivity.java
+++ trunk/src/com/mystic/peanut/radiation/RadiationMapperActivity.java
@@ -1,20 +1,58 @@
 package com.mystic.peanut.radiation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.telephony.NeighboringCellInfo;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class RadiationMapperActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	public String RM_TAG = "RadiationMapper";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         TelephonyManager telephonyManager = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
-        telephonyManager.getNeighboringCellInfo();
-        // for example value of first element
+        List<NeighboringCellInfo> myList = telephonyManager.getNeighboringCellInfo();
+        
+        List<String> cellStrList = new ArrayList<String>();
+        
+        if (myList.size()==0)
+        {
+        	Log.d(RM_TAG, "getNeighboringCellInfo returns empty list");
+        	//TextView textView = (TextView) findViewById(R.id.textview);
+        	//textView.setText("No Cells Available!");
+        	
+        	String cellStr = "No Cells Available!";
+        	cellStrList.add(cellStr);
+        }
+        else
+        {
+	        for (int i=0; i< myList.size(); i++)
+	        {
+	        	String cellStr = myList.get(i).toString();
+	        	cellStrList.add(cellStr);
+	        }
+        }
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cellStrList);
+        
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+        
+        
+        // for api 17 example value of first element
         //CellInfoGsm cellinfogsm = (CellInfoGsm)telephonyManager.getAllCellInfo().get(0);
         //CellSignalStrengthGsm cellSignalStrengthGsm = cellinfogsm.getCellSignalStrength();
         //cellSignalStrengthGsm.getDbm();
